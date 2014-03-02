d9ea5a7817022fb62ede6df4de5259137e23b4b6
David Roundy <roundyd@physics.oregonstate.edu>
2014-03-02 06:58:28 -0800

    create very hokey and simple (and insecure?) server Tested on darcy.

---
+++ server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"io"
+	"net"
+	"fmt"
+	"strings"
+	"time"
+)
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	var name string
+	n, err := fmt.Fscanln(c, &name)
+	if n != 1 || err != nil {
+		fmt.Println("unable to read from conn!\n", c.RemoteAddr())
+		return
+	}
+	if strings.ContainsAny(name, "/ \t\n") || strings.Count(name, "..") > 0 {
+		fmt.Println("bad name given: ", name)
+		return
+	}
+	os.MkdirAll("data/" + name, 0777)
+	f, err := os.Create("data/" + name + "/" + time.Now().Format("2006-01-02-15-04-05"))
+	if err != nil {
+		fmt.Println("unable to create file! ", err)
+		return
+	}
+	defer f.Close()
+	fmt.Println("Connection from ", name, " on IP address ", c.RemoteAddr())
+	io.Copy(f, c)
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":5432")
+	if err != nil {
+		// handle error
+		fmt.Println("Unable to create socket!\n");
+		os.Exit(1)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			continue
+		}
+		fmt.Println("reading from conn!\n", conn.RemoteAddr())
+		go handleConnection(conn)
+	}
+}
